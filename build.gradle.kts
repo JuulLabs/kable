103e78775015bd8ee8d0474aaa14ddc34d74d1c9
Travis Wyatt <travis@juul.com>
2020-09-04 23:18:51 -0700

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+buildscript {
+    repositories {
+        jcenter()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.4.0"))
+    }
+}
+
+subprojects {
+    repositories {
+        jcenter()
+    }
+}
