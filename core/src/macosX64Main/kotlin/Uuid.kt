d11e4e0acd03a9314c5a0eda7b69cb7dbc9eaa2e
Travis Wyatt <travis@juul.com>
2020-12-04 11:07:05 -0800

    Support Core Bluetooth 16-bit UUID representation (#15)

--- core/src/macosX64Main/kotlin/Uuid.kt
+++ core/src/macosX64Main/kotlin/Uuid.kt
@@ -7,4 +7,7 @@ import platform.Foundation.NSUUID
 
 internal fun Uuid.toNSUUID(): NSUUID = NSUUID(toString())
 internal fun Uuid.toCBUUID(): CBUUID = CBUUID.UUIDWithString(toString())
-internal fun CBUUID.toUuid(): Uuid = uuidFrom(UUIDString)
+internal fun CBUUID.toUuid(): Uuid = when (UUIDString.length) {
+    4 -> uuidFrom("0000$UUIDString-0000-1000-8000-00805F9B34FB")
+    else -> uuidFrom(UUIDString)
+}
