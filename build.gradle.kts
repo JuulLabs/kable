1d1fe7be270f5205262744625c1cbecefafd68f3
Justin Tiffner <justin.tiffner@juul.com>
2020-12-15 16:17:08 -0800

    Moving to dokkamultimodules.

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,8 +10,16 @@ plugins {
     id("com.android.library") version "4.0.2" apply false
     id("org.jmailen.kotlinter") version "3.2.0" apply false
     id("com.vanniktech.maven.publish") version "0.13.0" apply false
-    id("org.jetbrains.dokka") version "1.4.10.2" apply false
+    id("org.jetbrains.dokka") version "1.4.10.2"
     id("kotlinx-atomicfu") version "0.14.4" apply false
+}
+
+tasks.withType<org.jetbrains.dokka.gradle.DokkaMultiModuleTask>().configureEach {
+    val dokkaDir = buildDir.resolve("dokkaHtmlMultimodule")
+    outputDirectory.set(dokkaDir)
+    doLast {
+        dokkaDir.resolve("-modules.html").renameTo(dokkaDir.resolve("index.html"))
+    }
 }
 
 allprojects {
