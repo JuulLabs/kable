9c964a522b83f00bc38126031be1faab8b1cc868
burnhamd <burnhamd@gmail.com>
2021-06-24 19:47:25 -0400

    Update core/src/commonMain/kotlin/Scanner.kt
    Co-authored-by: Travis Wyatt <travis.i.wyatt@gmail.com>

--- core/src/commonMain/kotlin/Scanner.kt
+++ core/src/commonMain/kotlin/Scanner.kt
@@ -7,6 +7,4 @@ public interface Scanner {
     public val advertisements: Flow<Advertisement>
 }
 
-// todo: Add support for specifying services to filter against (for platforms that support it).
-// https://github.com/JuulLabs/kable/issues/22
 public expect fun Scanner(services: List<Uuid>? = null): Scanner
