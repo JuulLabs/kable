1d37814128a78c971d4a3f988335a8b79542faf4
Travis Wyatt <travis.i.wyatt@gmail.com>
2021-07-12 23:16:43 -0700

    Remove unnecessary typing for `convert`

--- core/src/appleMain/kotlin/NSData.kt
+++ core/src/appleMain/kotlin/NSData.kt
@@ -21,6 +21,6 @@ internal fun NSData.toByteArray(): ByteArray = ByteArray(length.toInt()).apply {
 internal fun ByteArray.toNSData(): NSData = memScoped {
     NSData.create(
         bytes = allocArrayOf(this@toNSData),
-        length = size.convert<NSUInteger>()
+        length = size.convert()
     )
 }
