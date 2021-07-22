97069df334d6ac4ce0c6ad28d01581d12167e330
Travis Wyatt <travis@juul.com>
2021-07-22 13:28:19 -0700

    Fix `requestPeripheral` when using Kotlin/JS IR compiler (#146)

--- core/src/jsMain/kotlin/Bluetooth.kt
+++ core/src/jsMain/kotlin/Bluetooth.kt
@@ -1,6 +1,10 @@
 package com.juul.kable
 
+import com.juul.kable.Options.Filter.Name
+import com.juul.kable.Options.Filter.NamePrefix
+import com.juul.kable.Options.Filter.Services
 import com.juul.kable.external.Bluetooth
+import kotlinext.js.jsObject
 import kotlinx.coroutines.CoroutineScope
 import kotlin.js.Promise
 
@@ -14,14 +18,40 @@ public fun CoroutineScope.requestPeripheral(
     .requestDevice(options.toDynamic())
     .then { device -> peripheral(device, builderAction) }
 
+/**
+ * Converts [Options] to JavaScript friendly object.
+ *
+ * According to the `requestDevice`
+ * [example](https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/requestDevice#example), the form of the
+ * JavaScript object should be similar to:
+ * ```
+ * let options = {
+ *   filters: [
+ *     {services: ['heart_rate']},
+ *     {services: [0x1802, 0x1803]},
+ *     {services: ['c48e6067-5295-48d3-8d5c-0395f61792b1']},
+ *     {name: 'ExampleName'},
+ *     {namePrefix: 'Prefix'}
+ *   ],
+ *   optionalServices: ['battery_service']
+ * }
+ * ```
+ */
 private fun Options.toDynamic(): dynamic = if (filters == null) {
-    object {
-        val acceptAllDevices = true
-        val optionalServices = this@toDynamic.optionalServices
+    jsObject {
+        this.acceptAllDevices = true
+        this.optionalServices = optionalServices
     }
 } else {
-    object {
-        val optionalServices = this@toDynamic.optionalServices
-        val filters = this@toDynamic.filters
+    jsObject {
+        this.optionalServices = optionalServices
+        this.filters = filters.map { it.toDynamic() }.toTypedArray()
     }
 }
+
+private fun Options.Filter.toDynamic(): dynamic =
+    when (this) {
+        is Name -> jsObject { this.name = name }
+        is NamePrefix -> jsObject { this.namePrefix = namePrefix }
+        is Services -> jsObject { this.services = services }
+    }
