d5c6a193a21c80e3862bc4c8b84cd5742718bf69
Travis Wyatt <travis@juul.com>
2021-12-04 12:51:30 -0800

    Enable logging of unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,4 +28,14 @@ allprojects {
         google()
         mavenCentral()
     }
+
+    tasks.withType<Test>().configureEach {
+        testLogging {
+            events("started", "passed", "skipped", "failed", "standardOut", "standardError")
+            exceptionFormat = org.gradle.api.tasks.testing.logging.TestExceptionFormat.FULL
+            showExceptions = true
+            showStackTraces = true
+            showCauses = true
+        }
+    }
 }
