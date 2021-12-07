93601715b55845b2d1e8fd12621a1e0869301265
David Taylor <david.taylor@juul.com>
2021-12-06 16:04:05 -0800

    Handle UUID aliases during service discovery

--- core/src/jsMain/kotlin/UUID.kt
+++ core/src/jsMain/kotlin/UUID.kt
@@ -2,6 +2,10 @@ package com.juul.kable
 
 import com.benasher44.uuid.Uuid
 import com.benasher44.uuid.uuidFrom
+import com.juul.kable.external.BluetoothUUID
+
+// Number of characters in a 16-bit UUID alias in string hex representation
+private const val UUID_ALIAS_STRING_LENGTH = 4
 
 /**
  * Per [Web Bluetooth](https://webbluetoothcg.github.io/web-bluetooth/#typedefdef-uuid) Draft Community Group Report,
@@ -13,4 +17,10 @@ import com.benasher44.uuid.uuidFrom
  */
 internal typealias UUID = String
 
-internal fun UUID.toUuid(): Uuid = uuidFrom(this)
+internal fun UUID.toUuid(): Uuid =
+    uuidFrom(
+        when (length) {
+            UUID_ALIAS_STRING_LENGTH -> BluetoothUUID.canonicalUUID(toInt(16))
+            else -> this
+        }
+    )
