e3a0ea922c833d6996e1d8e9c90e750f6f69f7c4
Mike Trewartha <4084686+mtrewartha@users.noreply.github.com>
2021-12-14 10:43:09 -0600

    Apply "@Deprecated" suggestions from code review
    Co-authored-by: Travis Wyatt <travis.i.wyatt@gmail.com>

--- core/src/appleMain/kotlin/ScannerBuilder.kt
+++ core/src/appleMain/kotlin/ScannerBuilder.kt
@@ -4,6 +4,13 @@ import com.juul.kable.logs.Logging
 import com.juul.kable.logs.LoggingBuilder
 
 public actual class ScannerBuilder {
+    @Deprecated(message = "Replaced by filters property")
+    public var services: List<Uuid>?
+        set(value) {
+            filters = value?.map { Filter.Service(it) }
+        }
+        get() = filters?.filterIsInstance<Filter.Service>()?.map { it.uuid }
+
     public actual var filters: List<Filter>? = null
     private var logging: Logging = Logging()
 
