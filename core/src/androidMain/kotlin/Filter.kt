6b1d3d90aaeafa839516aa7a560c48cea2144737
Mike Trewartha <mtrewartha@parsyl.com>
2021-12-15 09:20:17 -0600

    Fix a couple typos

--- core/src/androidMain/kotlin/Filter.kt
+++ core/src/androidMain/kotlin/Filter.kt
@@ -8,14 +8,14 @@ public actual sealed class Filter {
      * https://developer.android.com/reference/android/bluetooth/le/ScanFilter.Builder#setManufacturerData(int,%20byte[],%20byte[])
      */
     public class ManufacturerData(
-        /** A negative [id] is considered as invalid id. */
+        /** A negative [id] is considered an invalid id. */
         public val id: Int,
 
         public val data: ByteArray,
 
         /**
-         * For any bit in the mask, set it the 1 if it needs to match the one in [data], otherwise set it to 0.
-         * [dataMask] must have the same length as [data].
+         * For any bit in the mask, set it to 1 if it needs to match the corresponding bit in [data], otherwise set it
+         * to 0. [dataMask] must have the same length as [data].
          */
         public val dataMask: ByteArray?
     ) : Filter()
