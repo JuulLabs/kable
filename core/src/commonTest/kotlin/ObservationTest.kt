167a1adfb9fc1da5636a17af70ec2d24a950ba42
Travis Wyatt <travis@juul.com>
2022-01-11 23:05:08 -0800

    Additional tests and fixes

--- core/src/commonTest/kotlin/ObservationTest.kt
+++ core/src/commonTest/kotlin/ObservationTest.kt
@@ -3,7 +3,9 @@ package com.juul.kable
 import com.benasher44.uuid.uuid4
 import com.juul.kable.State.Connected
 import com.juul.kable.State.Disconnected
+import com.juul.kable.logs.LogEngine
 import com.juul.kable.logs.Logging
+import com.juul.kable.logs.Logging.Format.Compact
 import com.juul.kable.logs.Logging.Level.Data
 import com.juul.tuulbox.logging.ConsoleLogger
 import com.juul.tuulbox.logging.Log
@@ -14,6 +16,7 @@ import kotlin.test.AfterTest
 import kotlin.test.BeforeTest
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
 
 private fun generateCharacteristic() = characteristicOf(
     service = uuid4().toString(),
@@ -85,19 +88,20 @@ class ObservationTest {
         val characteristic = generateCharacteristic()
         val counter = ObservationCounter(characteristic)
         val observation = Observation(state, counter, characteristic, logging, identifier = "test")
+        val onSubscriptionActions = List(10) { suspend { } }
 
-        repeat(10) {
-            observation.onSubscription { }
+        onSubscriptionActions.forEach { action ->
+            observation.onSubscription(action)
         }
-        repeat(5) {
-            observation.onCompletion { }
+        onSubscriptionActions.take(5).forEach { action ->
+            observation.onCompletion(action)
         }
 
         state.value = Disconnected()
         observation.onConnectionLost()
 
-        repeat(5) {
-            observation.onCompletion { }
+        onSubscriptionActions.drop(5).forEach { action ->
+            observation.onCompletion(action)
         }
         counter.assert(
             startCount = 1,
@@ -148,7 +152,7 @@ class ObservationTest {
             stopCount = 0,
         )
 
-        // Simulate subscribers before state has been updated to `Connected`.
+        // Simulate additional subscribers before state has been updated to `Connected`.
         repeat(5) {
             observation.onSubscription { }
         }
@@ -204,6 +208,99 @@ class ObservationTest {
             stopCount = 0,
         )
     }
+
+    @Test
+    fun connectionDropsWhileConnecting_doesNotThrow() = runTest {
+        val state = MutableStateFlow(Disconnected())
+        val characteristic = generateCharacteristic()
+        val handler = object : Observation.Handler {
+            override suspend fun startObservation(characteristic: Characteristic) =
+                throw NotConnectedException()
+
+            override suspend fun stopObservation(characteristic: Characteristic) =
+                throw NotConnectedException()
+        }
+        val logEngine = RecordingLogEngine()
+        val logging = Logging().apply {
+            level = Data
+            format = Compact
+            engine = logEngine
+        }
+        val identifier = "test"
+        val observation = Observation(state, handler, characteristic, logging, identifier)
+
+        observation.onSubscription { }
+        observation.onConnected()
+
+        assertEquals(
+            expected = listOf(
+                RecordingLogEngine.Record.Verbose(
+                    throwable = null,
+                    tag = "Kable/Observation",
+                    message = "$identifier Suppressed failure: com.juul.kable.${NotConnectedException::class.simpleName}",
+                )
+            ),
+            actual = logEngine.records.toList()
+        )
+    }
+
+    @Test
+    fun failureDuringStartObservation_propagates() = runTest {
+        val state = MutableStateFlow(Disconnected())
+        val characteristic = generateCharacteristic()
+        val handler = object : Observation.Handler {
+            override suspend fun startObservation(characteristic: Characteristic) = error("start")
+            override suspend fun stopObservation(characteristic: Characteristic) {}
+        }
+        val observation = Observation(state, handler, characteristic, logging, identifier = "test")
+
+        observation.onSubscription { }
+        val failure = assertFailsWith<IllegalStateException> {
+            observation.onConnected()
+        }
+        assertEquals(
+            expected = "start",
+            actual = failure.message,
+        )
+    }
+
+    @Test
+    fun failureDuringStopObservation_propagates() = runTest {
+        val state = MutableStateFlow(Connected)
+        val characteristic = generateCharacteristic()
+        val handler = object : Observation.Handler {
+            override suspend fun startObservation(characteristic: Characteristic) {}
+            override suspend fun stopObservation(characteristic: Characteristic) = error("stop")
+        }
+        val observation = Observation(state, handler, characteristic, logging, identifier = "test")
+
+        val onSubscriptionAction = suspend { }
+        observation.onSubscription(onSubscriptionAction)
+        val failure = assertFailsWith<IllegalStateException> {
+            observation.onCompletion(onSubscriptionAction)
+        }
+        assertEquals(
+            expected = "stop",
+            actual = failure.message,
+        )
+    }
+
+    @Test
+    fun failureInSubscriptionAction_propagates() = runTest {
+        val state = MutableStateFlow(Connected)
+        val characteristic = generateCharacteristic()
+        val counter = ObservationCounter(characteristic)
+        val observation = Observation(state, counter, characteristic, logging, identifier = "test")
+
+        val onSubscriptionAction = suspend { error("action") }
+        val failure = assertFailsWith<IllegalStateException> {
+            observation.onSubscription(onSubscriptionAction)
+        }
+        assertEquals(
+            expected = "action",
+            actual = failure.message,
+        )
+    }
 }
 
 private class ObservationCounter(
@@ -235,5 +332,76 @@ private class ObservationCounter(
             actual = this.stopCount,
             message = "Stop observation count",
         )
+    }
+}
+
+private class RecordingLogEngine : LogEngine {
+
+    val records = mutableListOf<Record>()
+
+    sealed class Record {
+        abstract val throwable: Throwable?
+        abstract val tag: String
+        abstract val message: String
+
+        data class Verbose(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+
+        data class Debug(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+
+        data class Info(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+
+        data class Warn(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+
+        data class Error(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+
+        data class Assert(
+            override val throwable: Throwable?,
+            override val tag: String,
+            override val message: String,
+        ) : Record()
+    }
+
+    override fun verbose(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Verbose(throwable, tag, message)
+    }
+
+    override fun debug(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Debug(throwable, tag, message)
+    }
+
+    override fun info(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Info(throwable, tag, message)
+    }
+
+    override fun warn(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Warn(throwable, tag, message)
+    }
+
+    override fun error(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Error(throwable, tag, message)
+    }
+
+    override fun assert(throwable: Throwable?, tag: String, message: String) {
+        records += Record.Assert(throwable, tag, message)
     }
 }
