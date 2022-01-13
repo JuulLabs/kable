cd46759c60b7a952085a88b0b3d49855ca355a18
Travis Wyatt <travis@juul.com>
2022-01-13 13:55:32 -0800

    Code clean up

--- core/src/commonMain/kotlin/Observation.kt
+++ core/src/commonMain/kotlin/Observation.kt
@@ -25,7 +25,11 @@ internal class Observation(
 
     private val mutex = Mutex()
     private val subscribers = mutableListOf<OnSubscriptionAction>()
-    private val isObservationEnabled = atomic(false)
+
+    private val _isObservationEnabled = atomic(false)
+    private var isObservationEnabled: Boolean
+        get() = _isObservationEnabled.value
+        set(value) { _isObservationEnabled.value = value }
 
     private val isConnected: Boolean
         get() = state.value.isAtLeast<Observes>()
@@ -36,7 +40,7 @@ internal class Observation(
     suspend fun onSubscription(action: OnSubscriptionAction) = mutex.withLock {
         subscribers += action
         enableObservationIfNeeded()
-        if (isObservationEnabled.value) {
+        if (isObservationEnabled) {
             suppressConnectionExceptions {
                 action()
             }
@@ -51,7 +55,7 @@ internal class Observation(
     suspend fun onConnected() = mutex.withLock {
         // `force = true` is used in case a disconnected state (to reset the observation state) was missed.
         enableObservationIfNeeded(force = true)
-        if (isObservationEnabled.value) {
+        if (isObservationEnabled) {
             suppressConnectionExceptions {
                 subscribers.forEach { it() }
             }
@@ -60,24 +64,24 @@ internal class Observation(
 
     fun onConnectionLost() {
         // We assume that remote peripheral and local BLE system implicitly clears notifications/indications.
-        isObservationEnabled.value = false
+        isObservationEnabled = false
     }
 
     private suspend fun enableObservationIfNeeded(force: Boolean = false) {
-        if ((force || (!isObservationEnabled.value && isConnected)) && hasSubscribers) {
+        if ((!isObservationEnabled || force) && isConnected && hasSubscribers) {
             suppressConnectionExceptions {
                 handler.startObservation(characteristic)
-                isObservationEnabled.value = true
+                isObservationEnabled = true
             }
         }
     }
 
     private suspend fun disableObservationIfNeeded() {
-        if (isObservationEnabled.value && isConnected && !hasSubscribers) {
+        if (isObservationEnabled && isConnected && !hasSubscribers) {
             suppressConnectionExceptions {
                 handler.stopObservation(characteristic)
             }
-            isObservationEnabled.value = false
+            isObservationEnabled = false
         }
     }
 
