16ee31f48676c8aa2a62f173928344e44174864e
Travis Wyatt <travis@juul.com>
2022-01-13 23:45:54 -0800

    Remove unused import

--- core/src/androidMain/kotlin/Peripheral.kt
+++ core/src/androidMain/kotlin/Peripheral.kt
@@ -41,7 +41,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.withContext
