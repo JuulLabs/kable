JuulLabs/kable

commit 482b693c93ea0f33189db819ca9a7711c5df58b5

Add `Bluetooth.availability` flow (#335)

core/src/androidMain/kotlin/Bluetooth.kt
@@ -1,0 +1,65 @@
+package com.juul.kable
+
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothAdapter.ERROR
+import android.bluetooth.BluetoothAdapter.EXTRA_STATE
+import android.bluetooth.BluetoothAdapter.STATE_OFF
+import android.bluetooth.BluetoothAdapter.STATE_ON
+import android.bluetooth.BluetoothAdapter.STATE_TURNING_OFF
+import android.bluetooth.BluetoothAdapter.STATE_TURNING_ON
+import android.content.Context
+import android.content.IntentFilter
+import android.location.LocationManager
+import android.os.Build
+import androidx.core.content.ContextCompat
+import androidx.core.location.LocationManagerCompat
+import com.juul.kable.Bluetooth.Availability.Available
+import com.juul.kable.Bluetooth.Availability.Unavailable
+import com.juul.kable.Reason.LocationServicesDisabled
+import com.juul.kable.Reason.Off
+import com.juul.kable.Reason.TurningOff
+import com.juul.kable.Reason.TurningOn
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED as BLUETOOTH_STATE_CHANGED
+
+public actual enum class Reason {
+    Off, // BluetoothAdapter.STATE_OFF
+    TurningOff, // BluetoothAdapter.STATE_TURNING_OFF or BluetoothAdapter.STATE_BLE_TURNING_OFF
+    TurningOn, // BluetoothAdapter.STATE_TURNING_ON or BluetoothAdapter.STATE_BLE_TURNING_ON
+
+    /** Only applicable on Android 11 (API 30) and lower. */
+    LocationServicesDisabled,
+}
+
+private val Context.locationManager: LocationManager
+    get() = ContextCompat.getSystemService(this, LocationManager::class.java)
+        ?: error("LocationManager system service unavailable")
+
+private val isLocationEnabled: Boolean
+    get() = LocationManagerCompat.isLocationEnabled(applicationContext.locationManager)
+
+internal actual val bluetoothAvailability: Flow<Bluetooth.Availability> =
+    broadcastReceiverFlow(IntentFilter(BLUETOOTH_STATE_CHANGED))
+        .map { intent -> intent.getIntExtra(EXTRA_STATE, ERROR) }
+        .map { state ->
+            when (state) {
+                STATE_ON -> Available
+                STATE_OFF -> Unavailable(reason = Off)
+                STATE_TURNING_OFF -> Unavailable(reason = TurningOff)
+                STATE_TURNING_ON -> Unavailable(reason = TurningOn)
+                else -> error("Unexpected bluetooth state: $state")
+            }
+        }
+        .onStart {
+            val availability = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R && !isLocationEnabled) {
+                Unavailable(reason = LocationServicesDisabled)
+            } else {
+                when (BluetoothAdapter.getDefaultAdapter()?.isEnabled) {
+                    true -> Available
+                    else -> Unavailable(reason = Off)
+                }
+            }
+            emit(availability)
+        }

core/src/androidMain/kotlin/broadcastReceiverFlow.kt
@@ -1,0 +1,22 @@
+package com.juul.kable
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+
+public fun broadcastReceiverFlow(
+    intentFilter: IntentFilter,
+): Flow<Intent> = callbackFlow {
+    val broadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+            if (intent != null) trySend(intent)
+        }
+    }
+    ContextCompat.registerReceiver(applicationContext, broadcastReceiver, intentFilter, 0)
+    awaitClose { applicationContext.unregisterReceiver(broadcastReceiver) }
+}

core/src/appleMain/kotlin/Bluetooth.kt
@@ -1,0 +1,39 @@
+package com.juul.kable
+
+import com.juul.kable.Bluetooth.Availability.Available
+import com.juul.kable.Bluetooth.Availability.Unavailable
+import com.juul.kable.Reason.Off
+import com.juul.kable.Reason.Resetting
+import com.juul.kable.Reason.Unauthorized
+import com.juul.kable.Reason.Unknown
+import com.juul.kable.Reason.Unsupported
+import kotlinx.cinterop.UnsafeNumber
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import platform.CoreBluetooth.CBCentralManagerStatePoweredOff
+import platform.CoreBluetooth.CBCentralManagerStatePoweredOn
+import platform.CoreBluetooth.CBCentralManagerStateResetting
+import platform.CoreBluetooth.CBCentralManagerStateUnauthorized
+import platform.CoreBluetooth.CBCentralManagerStateUnsupported
+
+/** https://developer.apple.com/documentation/corebluetooth/cbmanagerstate */
+public actual enum class Reason {
+    Off, // CBManagerState.poweredOff
+    Resetting, // CBManagerState.resetting
+    Unauthorized, // CBManagerState.unauthorized
+    Unsupported, // CBManagerState.unsupported
+    Unknown, // CBManagerState.unknown
+}
+
+@OptIn(UnsafeNumber::class)
+internal actual val bluetoothAvailability: Flow<Bluetooth.Availability> =
+    CentralManager.Default.delegate.state.map { state ->
+        when (state) {
+            CBCentralManagerStatePoweredOn -> Available
+            CBCentralManagerStatePoweredOff -> Unavailable(reason = Off)
+            CBCentralManagerStateResetting -> Unavailable(reason = Resetting)
+            CBCentralManagerStateUnauthorized -> Unavailable(reason = Unauthorized)
+            CBCentralManagerStateUnsupported -> Unavailable(reason = Unsupported)
+            else -> Unavailable(reason = Unknown)
+        }
+    }

core/src/commonMain/kotlin/Bluetooth.kt
@@ -1,0 +1,21 @@
+package com.juul.kable
+
+import kotlinx.coroutines.flow.Flow
+
+public expect enum class Reason
+
+public object Bluetooth {
+
+    public sealed class Availability {
+
+        public object Available : Availability() {
+            override fun toString(): String = "Available"
+        }
+
+        public data class Unavailable(val reason: Reason?) : Availability()
+    }
+
+    public val availability: Flow<Availability> = bluetoothAvailability
+}
+
+internal expect val bluetoothAvailability: Flow<Bluetooth.Availability>

core/src/jsMain/kotlin/Bluetooth.kt
@@ -1,15 +1,57 @@
 package com.juul.kable
 
+import com.juul.kable.Bluetooth.Availability.Available
+import com.juul.kable.Bluetooth.Availability.Unavailable
 import com.juul.kable.Options.Filter.Name
 import com.juul.kable.Options.Filter.NamePrefix
 import com.juul.kable.Options.Filter.Services
-import com.juul.kable.external.Bluetooth
+import com.juul.kable.Reason.BluetoothUndefined
+import com.juul.kable.external.BluetoothAvailabilityChanged
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.await
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.onStart
+import org.w3c.dom.events.Event
 import kotlin.js.Promise
+import com.juul.kable.external.Bluetooth as JsBluetooth
+
+public actual enum class Reason {
+    /** `window.navigator.bluetooth` is undefined. */
+    BluetoothUndefined,
+}
+
+private const val AVAILABILITY_CHANGED = "availabilitychanged"
+
+internal actual val bluetoothAvailability: Flow<Bluetooth.Availability> = callbackFlow {
+    if (safeWebBluetooth == null) return@callbackFlow
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth/onavailabilitychanged
+    val listener: (Event) -> Unit = { event ->
+        val isAvailable = event.unsafeCast<BluetoothAvailabilityChanged>().value
+        trySend(if (isAvailable) Available else Unavailable(reason = null))
+    }
+
+    bluetooth.apply {
+        addEventListener(AVAILABILITY_CHANGED, listener)
+        awaitClose {
+            removeEventListener(AVAILABILITY_CHANGED, listener)
+        }
+    }
+}.onStart {
+    val availability = if (safeWebBluetooth == null) {
+        Unavailable(reason = BluetoothUndefined)
+    } else {
+        val isAvailable = bluetooth.getAvailability().await()
+        if (isAvailable) Available else Unavailable(reason = null)
+    }
+    emit(availability)
+}
 
 // Deliberately NOT cast `as Bluetooth` to avoid potential class name collisions.
 @Suppress("UnsafeCastFromDynamic")
-internal val bluetooth: Bluetooth
+internal val bluetooth: JsBluetooth
     get() = checkNotNull(safeWebBluetooth) { "Bluetooth unavailable" }
 
 // In a node build environment (e.g. unit test) there is no window, guard for that to avoid build errors.

core/src/jsMain/kotlin/external/Bluetooth.kt
@@ -5,6 +5,7 @@ import kotlin.js.Promise
 
 /** https://developer.mozilla.org/en-US/docs/Web/API/Bluetooth */
 internal abstract external class Bluetooth : EventTarget {
+    fun getAvailability(): Promise<Boolean>
     fun requestDevice(options: dynamic): Promise<BluetoothDevice>
     fun requestLEScan(options: dynamic): Promise<BluetoothScan>
 }

core/api/core.api
@@ -37,6 +37,30 @@ public final class com/juul/kable/AndroidScanner : com/juul/kable/Scanner {
 	public fun getAdvertisements ()Lkotlinx/coroutines/flow/Flow;
 }
 
+public final class com/juul/kable/Bluetooth {
+	public static final field INSTANCE Lcom/juul/kable/Bluetooth;
+	public final fun getAvailability ()Lkotlinx/coroutines/flow/Flow;
+}
+
+public abstract class com/juul/kable/Bluetooth$Availability {
+}
+
+public final class com/juul/kable/Bluetooth$Availability$Available : com/juul/kable/Bluetooth$Availability {
+	public static final field INSTANCE Lcom/juul/kable/Bluetooth$Availability$Available;
+	public fun toString ()Ljava/lang/String;
+}
+
+public final class com/juul/kable/Bluetooth$Availability$Unavailable : com/juul/kable/Bluetooth$Availability {
+	public fun <init> (Lcom/juul/kable/Reason;)V
+	public final fun component1 ()Lcom/juul/kable/Reason;
+	public final fun copy (Lcom/juul/kable/Reason;)Lcom/juul/kable/Bluetooth$Availability$Unavailable;
+	public static synthetic fun copy$default (Lcom/juul/kable/Bluetooth$Availability$Unavailable;Lcom/juul/kable/Reason;ILjava/lang/Object;)Lcom/juul/kable/Bluetooth$Availability$Unavailable;
+	public fun equals (Ljava/lang/Object;)Z
+	public final fun getReason ()Lcom/juul/kable/Reason;
+	public fun hashCode ()I
+	public fun toString ()Ljava/lang/String;
+}
+
 public final class com/juul/kable/BluetoothDisabledException : com/juul/kable/BluetoothException {
 	public fun <init> ()V
 }
@@ -51,6 +75,10 @@ public final class com/juul/kable/BondState : java/lang/Enum {
 	public static final field None Lcom/juul/kable/BondState;
 	public static fun valueOf (Ljava/lang/String;)Lcom/juul/kable/BondState;
 	public static fun values ()[Lcom/juul/kable/BondState;
+}
+
+public final class com/juul/kable/BroadcastReceiverFlowKt {
+	public static final fun broadcastReceiverFlow (Landroid/content/IntentFilter;)Lkotlinx/coroutines/flow/Flow;
 }
 
 public abstract interface class com/juul/kable/Characteristic {
@@ -298,6 +326,15 @@ public final class com/juul/kable/ProfileKt {
 	public static final fun getSignedWrite-G25LNqA (I)Z
 	public static final fun getWrite-G25LNqA (I)Z
 	public static final fun getWriteWithoutResponse-G25LNqA (I)Z
+}
+
+public final class com/juul/kable/Reason : java/lang/Enum {
+	public static final field LocationServicesDisabled Lcom/juul/kable/Reason;
+	public static final field Off Lcom/juul/kable/Reason;
+	public static final field TurningOff Lcom/juul/kable/Reason;
+	public static final field TurningOn Lcom/juul/kable/Reason;
+	public static fun valueOf (Ljava/lang/String;)Lcom/juul/kable/Reason;
+	public static fun values ()[Lcom/juul/kable/Reason;
 }
 
 public final class com/juul/kable/ScanFailedException : java/lang/IllegalStateException {

core/build.gradle.kts
@@ -60,6 +60,7 @@ kotlin {
             dependencies {
                 api(libs.kotlinx.coroutines.android)
                 implementation(libs.atomicfu)
+                implementation(libs.androidx.core)
                 implementation(libs.androidx.startup)
             }
         }

commit 1fc30e1367b8e85b78ffd618007ce3eb68e9ec63

Fix regression (crash) introduced in #335

core/src/androidMain/kotlin/Bluetooth.kt
@@ -10,7 +10,8 @@ import android.bluetooth.BluetoothAdapter.STATE_TURNING_ON
 import android.content.Context
 import android.content.IntentFilter
 import android.location.LocationManager
-import android.os.Build
+import android.os.Build.VERSION.SDK_INT
+import android.os.Build.VERSION_CODES.R
 import androidx.core.content.ContextCompat
 import androidx.core.location.LocationManagerCompat
 import com.juul.kable.Bluetooth.Availability.Available
@@ -53,7 +54,7 @@ internal actual val bluetoothAvailability: Flow<Bluetooth.Availability> =
             }
         }
         .onStart {
-            val availability = if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.R && !isLocationEnabled) {
+            val availability = if (SDK_INT <= R && !isLocationEnabled) {
                 Unavailable(reason = LocationServicesDisabled)
             } else {
                 when (BluetoothAdapter.getDefaultAdapter()?.isEnabled) {

core/src/androidMain/kotlin/broadcastReceiverFlow.kt
@@ -5,6 +5,7 @@ import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
 import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.RECEIVER_NOT_EXPORTED
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
@@ -17,6 +18,6 @@ public fun broadcastReceiverFlow(
             if (intent != null) trySend(intent)
         }
     }
-    ContextCompat.registerReceiver(applicationContext, broadcastReceiver, intentFilter, 0)
+    ContextCompat.registerReceiver(applicationContext, broadcastReceiver, intentFilter, RECEIVER_NOT_EXPORTED)
     awaitClose { applicationContext.unregisterReceiver(broadcastReceiver) }
 }