06456fd7503a3d7497c12534506a1c6e701d381e
Travis Wyatt <travis@juul.com>
2023-01-17 11:49:36 -0800

    Add `Bluetooth.BaseUuid` utility for 16 and 32 bit UUIDs (#421)

--- core/src/commonMain/kotlin/Bluetooth.kt
+++ core/src/commonMain/kotlin/Bluetooth.kt
@@ -1,10 +1,30 @@
 package com.juul.kable
 
+import com.benasher44.uuid.Uuid
 import kotlinx.coroutines.flow.Flow
 
 public expect enum class Reason
 
 public object Bluetooth {
+
+    /**
+     * Bluetooth Base UUID: `00000000-0000-1000-8000-00805F9B34FB`
+     *
+     * [Bluetooth Core Specification, Vol 3, Part B: 2.5.1 UUID](https://www.bluetooth.com/specifications/specs/?types=adopted&keyword=Core+Specification)
+     */
+    public object BaseUuid {
+
+        private const val mostSignificantBits = 4096L // 00000000-0000-1000
+        private const val leastSignificantBits = -9223371485494954757L // 8000-00805F9B34FB
+
+        public operator fun plus(shortUuid: Int): Uuid = plus(shortUuid.toLong())
+
+        /** @param shortUuid 32-bits (or less) short UUID (if larger than 32-bits, will be truncated to 32-bits). */
+        public operator fun plus(shortUuid: Long): Uuid =
+            Uuid(mostSignificantBits + (shortUuid and 0xFFFF_FFFF shl 32), leastSignificantBits)
+
+        override fun toString(): String = "00000000-0000-1000-8000-00805F9B34FB"
+    }
 
     public sealed class Availability {
 
