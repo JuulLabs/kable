c60344c80efeb8d562f991633987889196b47b21
Travis Wyatt <travis@juul.com>
2023-05-29 12:31:34 -0700

    Support `action`s that don't spawn any coroutines using `scope` provided to it

--- core/src/commonMain/kotlin/SharedRepeatableAction.kt
+++ core/src/commonMain/kotlin/SharedRepeatableAction.kt
@@ -13,19 +13,19 @@ import kotlin.coroutines.CoroutineContext
 /**
  * A mechanism for launching and awaiting a shared action ([Deferred]) repeatedly.
  *
- * The action is started by calling [getOrAsync]. Subsequent calls to [getOrAsync] will return the
- * same (i.e. shared) action if it is still executing (not [completed][Deferred.isCompleted]).
+ * The [action] is started by calling [getOrAsync]. Subsequent calls to [getOrAsync] will return the
+ * same (i.e. shared) [action] until cancelled or failure occurs.
  *
- * The [action] is passed a [scope][CoroutineScope] that can be used to spawn coroutines that
+ * The [action] is passed a [scope][CoroutineScope] that can be used to spawn coroutines that can
  * outlive the [action]. [getOrAsync] will continue to return the same action until either
- * [cancelled][cancel], or [action] has finished executing **and** all coroutines spawned from the
- * [scope][CoroutineScope] provided to [action] have completed.
+ * [cancelled][cancel], or a failures occurs in either the [action] or any coroutines spawned from
+ * the [scope][CoroutineScope] provided to [action].
  *
  * An exception thrown from [action] will cancel any coroutines spawned from the
  * [scope][CoroutineScope] that was provided to the [action].
  *
  * Calling [cancel] or [cancelAndJoin] will cancel the [action] and any coroutines created from the
- * [scope][CoroutineScope] provided to the [action]. Subsequent calls to [getOrAsync] will then
+ * [scope][CoroutineScope] provided to the [action]. A subsequent call to [getOrAsync] will then
  * start the [action] again.
  */
 internal class SharedRepeatableAction<T>(
@@ -33,11 +33,10 @@ internal class SharedRepeatableAction<T>(
     private val action: suspend (scope: CoroutineScope) -> T,
 ) {
 
-    // Hold a reference to both jobs because `action.parent` (which is `root`) becomes `null` when
-    // `action` completes before `root`.
     private class Jobs<T>(
         val root: Job,
         val action: Deferred<T>,
+        var isCancelled: Boolean = false,
     )
 
     private var jobs: Jobs<T>? = null
@@ -46,27 +45,30 @@ internal class SharedRepeatableAction<T>(
     fun getOrAsync(): Deferred<T> = guard.withLock {
         // ktlint-disable indent
         (
-            jobs?.takeUnless { it.root.isCompleted }
-                ?: run {
-                    val rootJob = Job(coroutineContext.job)
-                    val rootScope = CoroutineScope(coroutineContext + rootJob)
-                    val actionJob = rootScope.async { action(rootScope) }.apply {
-                        invokeOnCompletion { cause ->
-                            if (cause != null) this@SharedRepeatableAction.cancel()
-                        }
+            jobs?.takeUnless { it.isCancelled } ?: run {
+                val rootJob = Job(coroutineContext.job)
+                val rootScope = CoroutineScope(coroutineContext + rootJob)
+                val actionJob = rootScope.async { action(rootScope) }.apply {
+                    invokeOnCompletion { cause ->
+                        if (cause != null) this@SharedRepeatableAction.cancel()
                     }
-                    Jobs(rootJob, actionJob)
-                }.also { jobs = it }
+                }
+                Jobs(rootJob, actionJob)
+            }.also { jobs = it }
         ).action
         // ktlint-enable indent
     }
 
     fun cancel() {
-        guard.withLock { jobs }?.root?.cancel()
+        guard.withLock {
+            jobs?.apply { isCancelled = true }
+        }?.root?.cancel()
     }
 
     suspend fun cancelAndJoin() {
-        guard.withLock { jobs }?.root?.cancelAndJoin()
+        guard.withLock {
+            jobs?.apply { isCancelled = true }
+        }?.root?.cancelAndJoin()
     }
 }
 
