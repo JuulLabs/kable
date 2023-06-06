9ea31b0ecd24e58cbc6e3d53bb773e2be7e857e4
Travis Wyatt <travis@juul.com>
2023-06-06 11:38:25 -0700

    Expose `BluetoothDevice.type`

--- core/src/androidMain/kotlin/AndroidPeripheral.kt
+++ core/src/androidMain/kotlin/AndroidPeripheral.kt
@@ -1,5 +1,8 @@
 package com.juul.kable
 
+import android.Manifest
+import android.os.Build
+import androidx.annotation.RequiresPermission
 import kotlinx.coroutines.flow.StateFlow
 
 @Deprecated(
@@ -11,6 +14,46 @@ public typealias Priority = AndroidPeripheral.Priority
 public interface AndroidPeripheral : Peripheral {
 
     public enum class Priority { Low, Balanced, High }
+
+    public enum class Type {
+
+        /** https://developer.android.com/reference/android/bluetooth/BluetoothDevice#DEVICE_TYPE_CLASSIC */
+        Classic,
+
+        /**
+         * Low Energy - LE-only
+         * https://developer.android.com/reference/android/bluetooth/BluetoothDevice#DEVICE_TYPE_LE
+         */
+        LowEnergy,
+
+        /**
+         * Dual Mode - BR/EDR/LE
+         * https://developer.android.com/reference/android/bluetooth/BluetoothDevice#DEVICE_TYPE_DUAL
+         */
+        DualMode,
+
+        /** https://developer.android.com/reference/android/bluetooth/BluetoothDevice#DEVICE_TYPE_UNKNOWN */
+        Unknown,
+    }
+
+    /**
+     * Get the type of the peripheral.
+     *
+     * Per [Making Android BLE work — part 1: Clearing the cache](https://medium.com/@martijn.van.welie/making-android-ble-work-part-1-a736dcd53b02#42d8),
+     * when [type] is [Unknown][Type.Unknown], a scan should be performed before [connect].
+     *
+     * For apps targeting [R][Build.VERSION_CODES.R] or lower, [type] requires the
+     * [BLUETOOTH][Manifest.permission.BLUETOOTH] permission which can be gained with a simple
+     * `<uses-permission>` manifest tag.
+     *
+     * For apps targeting [S][Build.VERSION_CODES.S] or or higher, [type] requires the
+     * [BLUETOOTH_CONNECT][Manifest.permission.BLUETOOTH_CONNECT] permission which can be gained
+     * with `Activity.requestPermissions(String[], int)`.
+     */
+    @get:RequiresPermission(
+        anyOf = ["android.permission.BLUETOOTH", "android.permission.BLUETOOTH_CONNECT"],
+    )
+    public val type: Type
 
     /**
      * Returns the hardware address of this [AndroidPeripheral].
