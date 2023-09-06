876bc3ddcc9cadb94a540d35ba7a51a1988369db
Travis Wyatt <travis@juul.com>
2023-09-05 20:13:19 -0700

    Make `consumerId` safer

--- core/src/appleMain/kotlin/CentralBuilder.kt
+++ core/src/appleMain/kotlin/CentralBuilder.kt
@@ -1,5 +1,7 @@
 package com.juul.kable
 
+import kotlinx.atomicfu.locks.SynchronizedObject
+import kotlinx.atomicfu.locks.synchronized
 import platform.CoreBluetooth.CBCentralManagerOptionRestoreIdentifierKey
 import platform.Foundation.NSUUID
 import platform.Foundation.NSUserDefaults
@@ -7,14 +9,18 @@ import platform.Foundation.NSUserDefaults
 private const val CBCENTRALMANAGER_RESTORATION_ID = "kable-central-manager"
 private const val CBCENTRALMANAGER_CONSUMER_ID_KEY = "kable-central-manager-consumer-id"
 
+private val guard = SynchronizedObject()
+
 // This value is needed to ensure multiple instances of Kable running on the same iOS device do not
 // cross pollinate restored instances of CBCentralManager. The value will live for the lifetime of
 // the consuming app.
 private val consumerId: String
-    get() = NSUserDefaults.standardUserDefaults.stringForKey(CBCENTRALMANAGER_CONSUMER_ID_KEY)
-        ?: NSUUID().UUIDString().also {
-            NSUserDefaults.standardUserDefaults.setObject(it, CBCENTRALMANAGER_CONSUMER_ID_KEY)
-        }
+    get() = synchronized(guard) {
+        NSUserDefaults.standardUserDefaults.stringForKey(CBCENTRALMANAGER_CONSUMER_ID_KEY)
+            ?: NSUUID().UUIDString().also {
+                NSUserDefaults.standardUserDefaults.setObject(it, CBCENTRALMANAGER_CONSUMER_ID_KEY)
+            }
+    }
 
 public class CentralBuilder internal constructor() {
 
