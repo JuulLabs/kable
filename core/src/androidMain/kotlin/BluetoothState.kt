b465373da643d67031bac29c7735bbe09b23ec50
Paul <paul.escallier@juul.com>
2023-11-15 10:57:25 -0800

    Revert to using no exported flag whatsoever

--- core/src/androidMain/kotlin/BluetoothState.kt
+++ core/src/androidMain/kotlin/BluetoothState.kt
@@ -1,19 +1,31 @@
 package com.juul.kable
 
-import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED
+import android.bluetooth.BluetoothAdapter.ERROR
+import android.bluetooth.BluetoothAdapter.EXTRA_STATE
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
 import android.content.IntentFilter
-import androidx.core.content.ContextCompat.RECEIVER_NOT_EXPORTED
-import com.juul.tuulbox.coroutines.flow.broadcastReceiverFlow
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.shareIn
 
 private val intentFilter = IntentFilter(ACTION_STATE_CHANGED)
 
 @OptIn(DelicateCoroutinesApi::class)
-internal val bluetoothState = broadcastReceiverFlow(intentFilter, RECEIVER_NOT_EXPORTED)
-    .map { intent -> intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR) }
-    .shareIn(GlobalScope, started = WhileSubscribed(replayExpirationMillis = 0))
+internal val bluetoothState = callbackFlow {
+    val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val state = intent.getIntExtra(EXTRA_STATE, ERROR)
+            channel.trySend(state)
+        }
+    }
+    applicationContext.registerReceiver(receiver, intentFilter)
+    awaitClose {
+        applicationContext.unregisterReceiver(receiver)
+    }
+}.shareIn(GlobalScope, started = WhileSubscribed(replayExpirationMillis = 0))
