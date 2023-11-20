a7d6a0b29341c8effa28b1145831e9406b77c64f
Travis Wyatt <travis@juul.com>
2023-11-20 09:20:31 -0800

    Drop unnecessary visibility marker

--- exceptions/src/jsMain/kotlin/Exceptions.kt
+++ exceptions/src/jsMain/kotlin/Exceptions.kt
@@ -2,7 +2,7 @@
 
 package com.juul.kable
 
-public actual open class IOException public actual constructor(
+public actual open class IOException actual constructor(
     message: String?,
     cause: Throwable?,
 ) : Exception(message) {
