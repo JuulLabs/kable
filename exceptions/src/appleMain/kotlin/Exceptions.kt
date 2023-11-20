5c42e8aec77fd0db2f3c485b8cc3306339e59ea2
Travis Wyatt <travis@juul.com>
2023-11-20 10:13:09 -0800

    Fix `IOException` to not drop `cause`

--- exceptions/src/appleMain/kotlin/Exceptions.kt
+++ exceptions/src/appleMain/kotlin/Exceptions.kt
@@ -5,7 +5,7 @@ package com.juul.kable
 public actual open class IOException actual constructor(
     message: String?,
     cause: Throwable?,
-) : Exception(message) {
+) : Exception(message, cause) {
     public actual constructor() : this(null, null)
     public actual constructor(message: String?) : this(message, null)
     public actual constructor(cause: Throwable?) : this(null, cause)
