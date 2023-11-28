4a8155c44e6f1a9488ce51cb3be5460a1934783f
Travis Wyatt <travis@juul.com>
2023-11-27 16:07:05 -0800

    Use default hierarchy template (#599)

--- exceptions/build.gradle.kts
+++ exceptions/build.gradle.kts
@@ -5,95 +5,21 @@ plugins {
     id("com.vanniktech.maven.publish")
 }
 
-/* ```
- *   common
- *   |-- js
- *   |-- jvm
- *   '-- apple
- *       |-- ios
- *       '-- macos
- * ```
- */
 kotlin {
     explicitApi()
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
 
-    jvm()
-    js().browser()
-    iosX64()
-    macosArm64()
     iosArm64()
     iosSimulatorArm64()
+    iosX64()
+    js().browser()
+    jvm()
+    macosArm64()
     macosX64()
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-            }
-        }
-
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-
-        val jsTest by getting {
-            dependencies {
-                implementation(kotlin("test-js"))
-            }
-        }
-
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
-
-        val appleMain by creating {
-            dependsOn(commonMain)
-        }
-
-        val appleTest by creating
-
-        val macosX64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val macosX64Test by getting {
-            dependsOn(appleTest)
-        }
-
-        val macosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val macosArm64Test by getting {
-            dependsOn(appleTest)
-        }
-
-        val iosX64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosX64Test by getting {
-            dependsOn(appleTest)
-        }
-
-        val iosArm64Main by getting {
-            dependsOn(appleMain)
-        }
-
-        val iosArm64Test by getting {
-            dependsOn(appleTest)
-        }
-
-        val iosSimulatorArm64Main by getting {
-            dependsOn(appleMain)
-        }
-        val iosSimulatorArm64Test by getting {
-            dependsOn(appleTest)
+        commonTest.dependencies {
+            implementation(kotlin("test"))
         }
     }
 }
