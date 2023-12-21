088447481492c5e1c457879333a20687c99872a8
Travis Wyatt <travis@juul.com>
2023-12-21 00:41:01 -0800

    Remove unused import

--- core/src/androidMain/kotlin/BluetoothDeviceAndroidPeripheral.kt
+++ core/src/androidMain/kotlin/BluetoothDeviceAndroidPeripheral.kt
@@ -41,7 +41,6 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
