96c2e0778b9f851fd71b440061ea77bdd437d8e1
Travis Wyatt <travis@juul.com>
2024-02-27 10:58:29 -0800

    Remove workaround for `jvmToolchain` not being honored pre AGP 8.1.0-alpha09 (#636)

--- log-engine-tuulbox/build.gradle.kts
+++ log-engine-tuulbox/build.gradle.kts
@@ -28,13 +28,6 @@ kotlin {
 }
 
 android {
-    // Workaround (for `jvmToolchain` not being honored) needed until AGP 8.1.0-alpha09.
-    // https://kotlinlang.org/docs/gradle-configure-project.html#gradle-java-toolchains-support
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-
     compileSdk = libs.versions.android.compile.get().toInt()
     defaultConfig.minSdk = libs.versions.android.min.get().toInt()
 
