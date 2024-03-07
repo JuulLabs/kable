a5ec2a012cbb2d4a7846150a6ca0aa5943a53f4d
Cedrick Cooke <development@cedrickc.net>
2024-03-07 12:44:29 -0800

    Log engine for Khronicle logging

---
+++ log-engine-khronicle/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("multiplatform")
+    id("org.jmailen.kotlinter")
+    id("org.jetbrains.dokka")
+    id("com.vanniktech.maven.publish")
+}
+
+kotlin {
+    explicitApi()
+    jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
+
+    iosArm64()
+    iosX64()
+    js().browser()
+    macosArm64()
+    macosX64()
+    jvm()
+
+    sourceSets {
+        commonMain.dependencies {
+            api(project(":core"))
+            api(libs.khronicle.core)
+        }
+    }
+}
