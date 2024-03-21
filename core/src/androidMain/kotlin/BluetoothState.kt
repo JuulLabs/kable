b8efb9da369459b5f4a5ed8b12e92af7ae7ae3bb
Travis Wyatt <travis@juul.com>
2024-03-21 13:36:18 -0700

    Use Tuulbox's `broadcastReceiverFlow` for bluetooth state

--- core/src/androidMain/kotlin/BluetoothState.kt
+++ core/src/androidMain/kotlin/BluetoothState.kt
@@ -3,29 +3,17 @@ package com.juul.kable
 import android.bluetooth.BluetoothAdapter.ACTION_STATE_CHANGED
 import android.bluetooth.BluetoothAdapter.ERROR
 import android.bluetooth.BluetoothAdapter.EXTRA_STATE
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.Intent
 import android.content.IntentFilter
+import com.juul.tuulbox.coroutines.flow.broadcastReceiverFlow
 import kotlinx.coroutines.DelicateCoroutinesApi
 import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.shareIn
 
 private val intentFilter = IntentFilter(ACTION_STATE_CHANGED)
 
 @OptIn(DelicateCoroutinesApi::class)
-internal val bluetoothState = callbackFlow {
-    val receiver = object : BroadcastReceiver() {
-        override fun onReceive(context: Context, intent: Intent) {
-            val state = intent.getIntExtra(EXTRA_STATE, ERROR)
-            channel.trySend(state)
-        }
-    }
-    applicationContext.registerReceiver(receiver, intentFilter)
-    awaitClose {
-        applicationContext.unregisterReceiver(receiver)
-    }
-}.shareIn(GlobalScope, started = WhileSubscribed(replayExpirationMillis = 0))
+internal val bluetoothState = broadcastReceiverFlow(intentFilter)
+    .map { intent -> intent.getIntExtra(EXTRA_STATE, ERROR) }
+    .shareIn(GlobalScope, started = WhileSubscribed(replayExpirationMillis = 0))
