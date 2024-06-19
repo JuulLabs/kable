420b0e00ba16efd4580c7683c529b5d6f48cfcdb
David Taylor <david.taylor@juul.com>
2024-06-18 21:59:14 -0700

    Fix up tests

--- core/src/commonTest/kotlin/FilterPredicateSetTests.kt
+++ core/src/commonTest/kotlin/FilterPredicateSetTests.kt
@@ -1,15 +1,28 @@
 package com.juul.kable
 
 import com.benasher44.uuid.uuidFrom
-import com.juul.kable.FilterPredicateBuilder.Name.Exact
-import com.juul.kable.FilterPredicateBuilder.Name.Prefix
+import com.juul.kable.Filter.Name.Exact
+import com.juul.kable.Filter.Name.Prefix
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 class FilterPredicateSetTests {
 
     private val TEST_UUID_1 = uuidFrom("deadbeef-0000-0000-0000-000000000000")
     private val TEST_UUID_2 = uuidFrom("0badcafe-0000-0000-0000-000000000000")
+
+    @Test
+    fun matches_emptySetVsNothing_isTrue() {
+        val predicateSet = FilterPredicateSetBuilder().build()
+        assertTrue(predicateSet.matches())
+    }
+
+    @Test
+    fun matches_emptySetVsName_isTrue() {
+        val predicateSet = FilterPredicateSetBuilder().build()
+        assertTrue(predicateSet.matches(name = "bob"))
+    }
 
     @Test
     fun matches_serviceOrNameFilterVsNothing_isFalse() {
@@ -21,10 +34,7 @@ class FilterPredicateSetTests {
                 name = Exact("bob")
             }
         }.build()
-        assertEquals(
-            expected = false,
-            actual = predicateSet.matches(),
-        )
+        assertFalse(predicateSet.matches())
     }
 
     @Test
@@ -37,10 +47,7 @@ class FilterPredicateSetTests {
                 name = Exact("bob")
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(services = listOf(TEST_UUID_1)),
-        )
+        assertTrue(predicateSet.matches(services = listOf(TEST_UUID_1)))
     }
 
     @Test
@@ -53,10 +60,7 @@ class FilterPredicateSetTests {
                 name = Exact("bob")
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(name = "bob"),
-        )
+        assertTrue(predicateSet.matches(name = "bob"))
     }
 
     @Test
@@ -69,10 +73,7 @@ class FilterPredicateSetTests {
                 name = Exact("bob")
             }
         }.build()
-        assertEquals(
-            expected = false,
-            actual = predicateSet.matches(name = "b"),
-        )
+        assertFalse(predicateSet.matches(name = "b"))
     }
 
     @Test
@@ -86,10 +87,7 @@ class FilterPredicateSetTests {
                 name = Prefix("c")
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(name = "charlie"),
-        )
+        assertTrue(predicateSet.matches(name = "charlie"))
     }
 
     @Test
@@ -103,10 +101,7 @@ class FilterPredicateSetTests {
                 name = Prefix("c")
             }
         }.build()
-        assertEquals(
-            expected = false,
-            actual = predicateSet.matches(name = "bob"),
-        )
+        assertFalse(predicateSet.matches(name = "bob"))
     }
 
     @Test
@@ -120,10 +115,7 @@ class FilterPredicateSetTests {
                 name = Prefix("c")
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(services = listOf(TEST_UUID_1), name = "bob"),
-        )
+        assertTrue(predicateSet.matches(services = listOf(TEST_UUID_1), name = "bob"))
     }
 
     @Test
@@ -136,10 +128,7 @@ class FilterPredicateSetTests {
                 services = listOf(TEST_UUID_2)
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(services = listOf(TEST_UUID_1)),
-        )
+        assertTrue(predicateSet.matches(services = listOf(TEST_UUID_1)))
     }
 
     @Test
@@ -152,10 +141,7 @@ class FilterPredicateSetTests {
                 services = listOf(TEST_UUID_2)
             }
         }.build()
-        assertEquals(
-            expected = true,
-            actual = predicateSet.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)),
-        )
+        assertTrue(predicateSet.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)))
     }
 
     // One slightly complex filter to save tedium of building hundreds of tests
@@ -192,28 +178,23 @@ class FilterPredicateSetTests {
                 name = Exact("charlie")
             }
         }.build()
-        assertEquals(
-            expected = false,
+        assertFalse(
             actual = predicateSet.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)),
             message = "Expect false because all matching service filters require a name as well",
         )
-        assertEquals(
-            expected = true,
+        assertTrue(
             actual = predicateSet.matches(services = listOf(TEST_UUID_1), name = "xavier"),
             message = "Expect true because we match on uuid and name prefix in predicate A",
         )
-        assertEquals(
-            expected = true,
+        assertTrue(
             actual = predicateSet.matches(services = listOf(TEST_UUID_1), name = "alice"),
             message = "Expect true because we match on exact name in predicate E",
         )
-        assertEquals(
-            expected = false,
+        assertFalse(
             actual = predicateSet.matches(name = "yani"),
             message = "Expect false because matching name prefix in predicate B requires a service uuid as well",
         )
-        assertEquals(
-            expected = true,
+        assertTrue(
             actual = predicateSet.matches(manufacturerData = ManufacturerData(37, byteArrayOf(2))),
             message = "Expect true because we match manufacturer data in predicate C",
         )
--- core/src/commonTest/kotlin/FilterPredicateTests.kt
+++ core/src/commonTest/kotlin/FilterPredicateTests.kt
@@ -1,11 +1,11 @@
 package com.juul.kable
 
 import com.benasher44.uuid.uuidFrom
-import com.juul.kable.Filter.Name
-import com.juul.kable.FilterPredicateBuilder.Name.Exact
-import com.juul.kable.FilterPredicateBuilder.Name.Prefix
+import com.juul.kable.Filter.Name.Exact
+import com.juul.kable.Filter.Name.Prefix
 import kotlin.test.Test
-import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 import com.juul.kable.Filter.ManufacturerData as ManufacturerDataFilter
 
 class FilterPredicateTests {
@@ -15,47 +15,32 @@ class FilterPredicateTests {
 
     @Test
     fun matches_nameFilterVsExactMatch_isTrue() {
-        val predicate = Name("bob").toPredicate()
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(name = "bob"),
-        )
+        val predicate = Exact("bob").toPredicate()
+        assertTrue(predicate.matches(name = "bob"))
     }
 
     @Test
     fun matches_nameFilterVsWrongName_isFalse() {
-        val predicate = Name("bob").toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(name = "bobby"),
-        )
+        val predicate = Exact("bob").toPredicate()
+        assertFalse(predicate.matches(name = "bobby"))
     }
 
     @Test
     fun matches_nameFilterVsNothing_isFalse() {
-        val predicate = Name("bob").toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(),
-        )
+        val predicate = Exact("bob").toPredicate()
+        assertFalse(predicate.matches())
     }
 
     @Test
     fun matches_nameFilterVsService_isFalse() {
-        val predicate = Name("bob").toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(services = listOf(TEST_UUID_1)),
-        )
+        val predicate = Exact("bob").toPredicate()
+        assertFalse(predicate.matches(services = listOf(TEST_UUID_1)))
     }
 
     @Test
     fun matches_nameFilterVsServiceAndName_isTrue() {
-        val predicate = Name("bob").toPredicate()
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(services = listOf(TEST_UUID_1), name = "bob"),
-        )
+        val predicate = Exact("bob").toPredicate()
+        assertTrue(predicate.matches(services = listOf(TEST_UUID_1), name = "bob"))
     }
 
     @Test
@@ -64,10 +49,7 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(services = listOf(TEST_UUID_1), name = "bob"),
-        )
+        assertTrue(predicate.matches(services = listOf(TEST_UUID_1), name = "bob"))
     }
 
     @Test
@@ -76,10 +58,7 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(),
-        )
+        assertFalse(predicate.matches())
     }
 
     @Test
@@ -88,10 +67,7 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(services = listOf(TEST_UUID_1)),
-        )
+        assertFalse(predicate.matches(services = listOf(TEST_UUID_1)))
     }
 
     @Test
@@ -100,10 +76,7 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(name = "bob"),
-        )
+        assertFalse(predicate.matches(name = "bob"))
     }
 
     @Test
@@ -112,22 +85,16 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(name = "b"),
-        )
+        assertFalse(predicate.matches(name = "b"))
     }
 
     @Test
-    fun matches_serviceAndNamePrefixFilterVsNamePrefix_isTrue() {
+    fun matches_serviceAndNamePrefixFilterVsNamePrefixOnly_isFalse() {
         val predicate = FilterPredicateBuilder().apply {
             services = listOf(TEST_UUID_1)
             name = Prefix("b")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(name = "bob"),
-        )
+        assertFalse(predicate.matches(name = "bob"))
     }
 
     @Test
@@ -136,10 +103,7 @@ class FilterPredicateTests {
             services = listOf(TEST_UUID_1)
             name = Exact("bob")
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(manufacturerData = ManufacturerData(42, byteArrayOf(1))),
-        )
+        assertFalse(predicate.matches(manufacturerData = ManufacturerData(42, byteArrayOf(1))))
     }
 
     @Test
@@ -147,10 +111,7 @@ class FilterPredicateTests {
         val predicate = FilterPredicateBuilder().apply {
             services = listOf(TEST_UUID_1, TEST_UUID_2)
         }.build()!!
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(services = listOf(TEST_UUID_1)),
-        )
+        assertFalse(predicate.matches(services = listOf(TEST_UUID_1)))
     }
 
     @Test
@@ -158,10 +119,7 @@ class FilterPredicateTests {
         val predicate = FilterPredicateBuilder().apply {
             services = listOf(TEST_UUID_1, TEST_UUID_2)
         }.build()!!
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)),
-        )
+        assertTrue(predicate.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)))
     }
 
     @Test
@@ -169,56 +127,38 @@ class FilterPredicateTests {
         val predicate = FilterPredicateBuilder().apply {
             services = listOf(TEST_UUID_1)
         }.build()!!
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)),
-        )
+        assertTrue(predicate.matches(services = listOf(TEST_UUID_1, TEST_UUID_2)))
     }
 
     @Test
     fun matches_manufacturerDataFilterVsExactMatch_isTrue() {
         val predicate = ManufacturerDataFilter(37, byteArrayOf(2)).toPredicate()
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(2))),
-        )
+        assertTrue(predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(2))))
     }
 
     @Test
     fun matches_manufacturerDataFilterVsIdMisMatch_isFalse() {
         val predicate = ManufacturerDataFilter(37, byteArrayOf(2)).toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(manufacturerData = ManufacturerData(31, byteArrayOf(2))),
-        )
+        assertFalse(predicate.matches(manufacturerData = ManufacturerData(31, byteArrayOf(2))))
     }
 
     @Test
     fun matches_manufacturerDataFilterVsEmptyData_isFalse() {
         val predicate = ManufacturerDataFilter(37, byteArrayOf(2)).toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(manufacturerData = ManufacturerData(31, byteArrayOf())),
-        )
+        assertFalse(predicate.matches(manufacturerData = ManufacturerData(31, byteArrayOf())))
     }
 
     @Test
     fun matches_manufacturerDataFilterVsDataMisMatchWithoutMask_isFalse() {
         val predicate = ManufacturerDataFilter(37, byteArrayOf(2)).toPredicate()
-        assertEquals(
-            expected = false,
-            actual = predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(3))),
-        )
+        assertFalse(predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(3))))
     }
 
     @Test
     fun matches_manufacturerDataFilterVsDataMatchWithMask_isTrue() {
         // Mask to match against only the single bit
         val predicate = ManufacturerDataFilter(37, byteArrayOf(2), byteArrayOf(2)).toPredicate()
-        assertEquals(
-            expected = true,
-            actual = predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(3))),
-        )
+        assertTrue(predicate.matches(manufacturerData = ManufacturerData(37, byteArrayOf(3))))
     }
 }
 
