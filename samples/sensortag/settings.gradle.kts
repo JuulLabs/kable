3c39f7a22097870f02a55bbb04eb1f54a5860fb2
Travis Wyatt <travis@juul.com>
2024-07-26 01:28:55 -0700

    Add support for including Kable as a composite dependency (#363)

--- samples/sensortag/settings.gradle.kts
+++ samples/sensortag/settings.gradle.kts
@@ -13,3 +13,22 @@ pluginManagement {
 }
 
 include("app")
+
+// Configure sibling composite projects (`../<project>`) by adding `composite.<project>=true` to `local.properties`.
+java.util.Properties()
+    .apply {
+        rootProject.projectDir
+            .resolve("local.properties")
+            .normalize()
+            .takeIf(File::exists)
+            ?.let { java.io.FileInputStream(it) }
+            ?.use(::load)
+    }
+    .run {
+        stringPropertyNames()
+            .filter { it.startsWith("composite.") && getProperty(it).toBoolean() }
+            .map { it.substringAfter('.') }
+            .onEach { logger.lifecycle("Including '$it' as a composite build") }
+            .map { "../$it" }
+            .forEach(::includeBuild)
+    }
