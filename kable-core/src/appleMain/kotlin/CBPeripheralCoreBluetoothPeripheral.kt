b6c7fad1491ba13eb55112bcc36f2a8d70f48d4c
Travis Wyatt <travis@juul.com>
2024-12-20 14:23:18 -0800

    Change `CancellationException` import to fix "overload resolution ambiguity"

--- kable-core/src/appleMain/kotlin/CBPeripheralCoreBluetoothPeripheral.kt
+++ kable-core/src/appleMain/kotlin/CBPeripheralCoreBluetoothPeripheral.kt
@@ -13,6 +13,8 @@ import com.juul.kable.logs.Logger
 import com.juul.kable.logs.Logging
 import com.juul.kable.logs.Logging.DataProcessor.Operation.Write
 import com.juul.kable.logs.detail
+import kotlin.time.Duration
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -43,8 +45,6 @@ import platform.Foundation.NSString
 import platform.Foundation.NSUTF8StringEncoding
 import platform.Foundation.dataUsingEncoding
 import platform.darwin.UInt16
-import kotlin.coroutines.cancellation.CancellationException
-import kotlin.time.Duration
 import platform.CoreBluetooth.CBCharacteristicWriteWithResponse as CBWithResponse
 import platform.CoreBluetooth.CBCharacteristicWriteWithoutResponse as CBWithoutResponse
 
