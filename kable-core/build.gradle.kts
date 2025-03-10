06b084cd96eee9246799eb9daf6d29feb2a0e034
Cristian <cristian.menarguez.gonzalez@gmail.com>
2025-03-10 15:50:05 +0100

    Added some changes

--- kable-core/build.gradle.kts
+++ kable-core/build.gradle.kts
@@ -9,6 +9,15 @@ plugins {
     id("com.vanniktech.maven.publish")
 }
 
+publishing {
+    repositories {
+        maven {
+            name = "LocalRepo"
+            url = uri(project.rootDir.resolve("repo").absolutePath)
+        }
+    }
+}
+
 kotlin {
     explicitApi()
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
--- kable-exceptions/build.gradle.kts
+++ kable-exceptions/build.gradle.kts
@@ -5,6 +5,16 @@ plugins {
     id("com.vanniktech.maven.publish")
 }
 
+
+publishing {
+    repositories {
+        maven {
+            name = "LocalRepo"
+            url = uri(project.rootDir.resolve("repo").absolutePath)
+        }
+    }
+}
+
 kotlin {
     explicitApi()
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
