54be04e26f29f05c1c497204c4d23c5a767211ce
Travis Wyatt <travis@juul.com>
2025-04-02 10:24:58 -0700

    Enable scanning on Topaz browser (#450)

--- samples/sensortag/app/src/jsMain/kotlin/features/scan/DeviceLocator.kt
+++ samples/sensortag/app/src/jsMain/kotlin/features/scan/DeviceLocator.kt
@@ -2,8 +2,17 @@ package com.juul.sensortag.features.scan
 
 import cafe.adriel.voyager.core.model.ScreenModel
 import cafe.adriel.voyager.core.model.screenModelScope
+import kotlinx.browser.window
 
 actual fun ScreenModel.DeviceLocator(
     onRequestDeviceSuccess: suspend () -> Unit,
     onStatus: suspend (String?) -> Unit,
-): DeviceLocator = RequestDeviceLocator(screenModelScope, onRequestDeviceSuccess, onStatus)
+): DeviceLocator = if (browserSupportsScanning) {
+    ScannerDeviceLocator(screenModelScope, onStatus)
+} else {
+    RequestDeviceLocator(screenModelScope, onRequestDeviceSuccess, onStatus)
+}
+
+private val browserSupportsScanning: Boolean
+    // https://github.com/JuulLabs/topaz supports scanning.
+    get() = window.navigator.userAgent.contains("topaz", ignoreCase = true)
