8c6e645c25895ae3ae065721469e082cc65a4168
Cedrick Cooke <development@cedrickc.net>
2025-04-24 13:07:33 -0700

    Prove that rust's async runtime is compatible with kotlin

--- kable-btleplug-ffi/src/commonMain/rust/lib.rs
+++ kable-btleplug-ffi/src/commonMain/rust/lib.rs
@@ -2,5 +2,7 @@ uniffi::setup_scaffolding!();
 
 #[uniffi::export(async_runtime = "tokio")]
 pub async fn hello_world() -> String {
+    // Sleep is just used to verify that the async runtime is working.
+    tokio::time::sleep(tokio::time::Duration::from_millis(1000)).await;
     "Hello, world!".to_owned()
 }
