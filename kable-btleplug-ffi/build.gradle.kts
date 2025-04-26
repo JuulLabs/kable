a33eaca19a400904590ef1b6d21d9c1974d5e81d
Cedrick Cooke <development@cedrickc.net>
2025-04-25 19:55:54 -0700

    Initial stab at multiplatform builds

--- kable-btleplug-ffi/build.gradle.kts
+++ kable-btleplug-ffi/build.gradle.kts
@@ -2,10 +2,6 @@ plugins {
     kotlin("multiplatform")
 }
 
-// TODO: this currently is only configured for desktop mac (.dylib), but should
-//       also support linux (.so) and windows (.dll) targets. Ideally, all of
-//       those can build from every platform.
-
 kotlin {
     // Uniffi-bindgen is not explicitApi() compatible
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
@@ -41,6 +37,58 @@ fun Task.commonRustSetup() {
     }
 }
 
+data class Target(
+    val name: String,
+    val triple: String,
+    val libraryName: String,
+)
+
+val targets = run {
+    val currentOs = run {
+        val name = System.getProperty("os.name").lowercase()
+        when {
+            "mac" in name -> "apple"
+            "windows" in name -> "windows"
+            else -> "linux"
+        }
+    }
+    val currentArch = run {
+        val arch = System.getProperty("os.arch").lowercase()
+        when {
+            "arm" in arch || "aarch" in arch -> "aarch64"
+            else -> "x86_64"
+        }
+    }
+    listOf(
+        Target("MacosArm64", "aarch64-apple-darwin", "libbtleplug_ffi.dylib"),
+        Target("MacosX64", "x86_64-apple-darwin", "btleplug_ffi.dylib"),
+        Target("WindowsX64", "x86_64-pc-windows-msvc", "btleplug_ffi.dll"),
+        Target("LinuxX64", "x86_64-unknown-linux-gnu", "libbtleplug_ffi.so"),
+        Target("LinuxArm64", "aarch64-unknown-linux-gnu", "libbtleplug_ffi.so"),
+    ).filter { target ->
+        currentOs.contains("mac") || (currentOs in target.triple && currentArch in target.triple)
+    }
+}
+
+targets.forEach { target ->
+    tasks.register<Exec>("cargoInstallTarget${target.name}") {
+        commonRustSetup()
+        commandLine("rustup", "target", "add", target.triple)
+        onlyIf { !File("~/.rustup/toolchains/stable-${target.triple}").exists() }
+    }
+
+    tasks.register<Exec>("cargoBuild${target.name}") {
+        commonRustSetup()
+        dependsOn("cargoInstallTarget${target.name}")
+        outputs.dir("target/${target.triple}/release")
+        commandLine(
+            "cargo", "build",
+            "--release",
+            "--target", target.triple,
+        )
+    }
+}
+
 tasks.register<Exec>("cargoClean") {
     commonRustSetup()
     commandLine("cargo", "clean")
@@ -58,6 +106,7 @@ tasks.register<Exec>("cargoTest") {
 
 tasks.register<Exec>("cargoBuild") {
     commonRustSetup()
+    dependsOn(targets.map { "cargoBuild${it.name}" })
     outputs.dir("target")
     commandLine("cargo", "build", "--release")
 }
@@ -86,7 +135,8 @@ tasks.register<Exec>("cargoUniffiBindgen") {
     commonRustSetup()
     dependsOn("cargoBuild")
     doFirst { project.delete("build/generated/uniffi/kotlin") }
-    inputs.file("target/release/libbtleplug_ffi.dylib")
+    val input = "target/${targets.first().triple}/release/${targets.first().libraryName}"
+    inputs.file(input)
     outputs.dir("build/generated/uniffi/kotlin")
     commandLine(
         "cargo",
@@ -96,7 +146,7 @@ tasks.register<Exec>("cargoUniffiBindgen") {
         "uniffi-bindgen",
         "generate",
         "--library",
-        "target/release/libbtleplug_ffi.dylib",
+        input,
         "--language",
         "kotlin",
         "--out-dir",
@@ -118,9 +168,20 @@ tasks.named("compileKotlinJvm") {
 }
 
 tasks.named<Copy>("jvmProcessResources") {
-    dependsOn("cargoUniffiBindgen")
+    dependsOn("cargoBuild")
 
-    from(project.file("target/release")) {
-        include("libbtleplug_ffi.dylib")
+    targets.forEach { target ->
+        from(project.file("target/${target.triple}/release")) {
+            include(target.libraryName)
+            val (os, arch) = when (target.name) {
+                "MacosArm64" -> "darwin" to "aarch64"
+                "MacosX64" -> "darwin" to "x86-64"
+                "WindowsX64" -> "win32" to "x86-64"
+                "LinuxX64" -> "linux" to "x86-64"
+                "LinuxArm64" -> "linux" to "aarch64"
+                else -> error("Unknown target: ${target.name}")
+            }
+            into("${os}-${arch}")
+        }
     }
 }
