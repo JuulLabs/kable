4124c37a3d19e77ebf716a4902e2f9839866c69a
Travis Wyatt <travis@juul.com>
2026-05-12 13:20:41 -0700

    Only declare native targets on MacOS systems

--- kable-core/build.gradle.kts
+++ kable-core/build.gradle.kts
@@ -8,17 +8,26 @@ plugins {
     kotlin("multiplatform")
 }
 
+fun isRunningOnMacOs() = System.getProperty("os.name").orEmpty().lowercase().startsWith("mac")
+
 kotlin {
     explicitApi()
     jvmToolchain(libs.versions.jvm.toolchain.get().toInt())
 
+    // Build fails on Linux ARM64 host (when building Rust bindings for JAR distribution), so we
+    // explicitly only include Native targets when running on MacOS.
+    // https://youtrack.jetbrains.com/issue/KT-36871
+    // https://youtrack.jetbrains.com/issue/KT-42445
+    if (isRunningOnMacOs()) {
+        iosArm64()
+        iosSimulatorArm64()
+        iosX64()
+        macosArm64()
+        macosX64()
+    }
+
     androidTarget().publishLibraryVariants("debug", "release")
-    iosArm64()
-    iosSimulatorArm64()
-    iosX64()
     js().browser()
-    macosArm64()
-    macosX64()
     jvm()
     wasmJs().browser()
 
