fde0c74495fad44c279799d9f59f10bc137e0064
Travis Wyatt <travis@juul.com>
2026-06-06 22:37:40 -0700

    Nest android config in kotlin block

--- kable-default-permissions/build.gradle.kts
+++ kable-default-permissions/build.gradle.kts
@@ -3,8 +3,10 @@ plugins {
     alias(libs.plugins.maven.publish)
 }
 
-android {
-    compileSdk = libs.versions.android.compile.get().toInt()
-    defaultConfig.minSdk = libs.versions.android.min.get().toInt()
-    namespace = "com.juul.kable.permissions"
+kotlin {
+    android {
+        compileSdk = libs.versions.android.compile.get().toInt()
+        defaultConfig.minSdk = libs.versions.android.min.get().toInt()
+        namespace = "com.juul.kable.permissions"
+    }
 }
