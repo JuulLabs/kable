b5ebafbe730572be24e7f1b34822381961c2b7d6
Travis Wyatt <travis@juul.com>
2026-06-09 11:18:33 -0700

    Drop usage of AtomicFu plugin (#1163)

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.kotlinter) apply false
     alias(libs.plugins.maven.publish) apply false
-    alias(libs.plugins.atomicfu) apply false
     alias(libs.plugins.dokka)
     alias(libs.plugins.api)
 }
