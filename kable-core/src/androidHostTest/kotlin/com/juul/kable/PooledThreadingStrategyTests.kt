76cfef49f4266622b554cfc741ddcd507fc05030
Travis Wyatt <travis@juul.com>
2026-07-15 12:07:41 -0700

    Clean up

--- kable-core/src/androidHostTest/kotlin/com/juul/kable/PooledThreadingStrategyTests.kt
+++ kable-core/src/androidHostTest/kotlin/com/juul/kable/PooledThreadingStrategyTests.kt
@@ -1,8 +1,7 @@
 package com.juul.kable
 
 import android.os.Build
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
+import kotlinx.coroutines.test.runTest
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
 import org.robolectric.annotation.Config
@@ -11,13 +10,12 @@ import kotlin.test.assertNotSame
 import kotlin.test.assertSame
 
 @RunWith(RobolectricTestRunner::class)
-@Config(sdk = [Build.VERSION_CODES.S])
+@Config(sdk = [Build.VERSION_CODES.M])
 class PooledThreadingStrategyTests {
 
     @Test
-    fun acquire_emptyPool_createsNewThreading() {
-        val job = Job()
-        val strategy = PooledThreadingStrategy(scope = CoroutineScope(job))
+    fun acquire_emptyPool_createsNewThreading() = runTest {
+        val strategy = PooledThreadingStrategy(scope = backgroundScope)
         try {
             val first = strategy.acquire()
             val second = strategy.acquire()
@@ -30,9 +28,8 @@ class PooledThreadingStrategyTests {
     }
 
     @Test
-    fun acquire_afterRelease_reusesPooledThreading() {
-        val job = Job()
-        val strategy = PooledThreadingStrategy(scope = CoroutineScope(job))
+    fun acquire_afterRelease_reusesPooledThreading() = runTest {
+        val strategy = PooledThreadingStrategy(scope = backgroundScope)
         try {
             val threading = strategy.acquire()
             strategy.release(threading)
