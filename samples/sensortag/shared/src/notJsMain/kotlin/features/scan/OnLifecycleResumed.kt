c8b45cffbe18d04e84f64b68ee8a7398c5d0c251
Travis Wyatt <travis@juul.com>
2026-09-08 22:33:50 -0700

    Fix imports

--- samples/sensortag/shared/src/notJsMain/kotlin/features/scan/OnLifecycleResumed.kt
+++ samples/sensortag/shared/src/notJsMain/kotlin/features/scan/OnLifecycleResumed.kt
@@ -4,8 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
 
 @Composable
 actual fun onLifecycleResumed(onResumed: () -> Unit) {
