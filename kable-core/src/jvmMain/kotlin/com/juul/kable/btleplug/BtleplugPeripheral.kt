4bc4e7d0aba13143c1d1c0923405e7ed9c9336b2
AvikMakwana <avikmakwana81@gmail.com>
2026-09-24 13:08:12 +0530

    Use ensureActive for MTU fallback

--- kable-core/src/jvmMain/kotlin/com/juul/kable/btleplug/BtleplugPeripheral.kt
+++ kable-core/src/jvmMain/kotlin/com/juul/kable/btleplug/BtleplugPeripheral.kt
@@ -34,6 +34,8 @@ import kotlinx.coroutines.NonCancellable
 import kotlinx.coroutines.awaitCancellation
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.coroutines.ensureActive
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
@@ -186,9 +188,9 @@ internal class BtleplugPeripheral(
             withContext(Dispatchers.IO) {
                 ffi.mtu().toInt() - ATT_MTU_HEADER_SIZE
             }
-        } catch (e: CancellationException) {
-            throw e
-        } catch (_: Exception) {
+        } catch (e: Exception) {
+            currentCoroutineContext().ensureActive()
+            logger.warn(e) { message = "Failed to get MTU, using default" }
             DEFAULT_ATT_MTU - ATT_MTU_HEADER_SIZE
         }
 
